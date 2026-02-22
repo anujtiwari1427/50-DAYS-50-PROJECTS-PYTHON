4b61df7e00814c9a2642ccfce228cb3c1bd3d6b7
ANUJ TIWARI <rajeshtiwari0232@gmail.com>
2026-02-22 19:45:58 +0530

    Delete DAY 12 directory

--- DAY 12/TRAFFIC_CONTROLL.py
+++
@@ -1,9 +0,0 @@
-vehicles = int(input("Enter number of vehicles: "))
-
-if vehicles > 50:
-    print("Green light: 60 seconds")
-elif vehicles > 20:
-    print("Green light: 40 seconds")
-else:
-    print("Green light: 20 seconds")
-    
